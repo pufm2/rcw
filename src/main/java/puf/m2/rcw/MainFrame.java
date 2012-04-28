46299e710c95bf71a5aba2cf1d3216b1ad30b952
lxhoan <lxhoan@gmail.com>
2012-04-28 18:09:22 +0700

    update UI, update report

--- src/main/java/puf/m2/rcw/MainFrame.java
+++ src/main/java/puf/m2/rcw/MainFrame.java
@@ -1,10 +1,9 @@
 package puf.m2.rcw;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JFileChooser;
 import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
 import javax.swing.SpringLayout;
@@ -19,6 +18,11 @@ import puf.m2.rcw.term.Term;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 
 public class MainFrame extends JFrame {
 
@@ -28,15 +32,17 @@ public class MainFrame extends JFrame {
     private JTextField textEdges;
     private JTextField textReducedTerm;
     private JTextField textTerm;
+    private JTextField textUsedLabels;
 
-
+    private final JFileChooser fc = new JFileChooser();
+    private final MainFrame me = this;
     /**
      * Create the frame.
      */
     public MainFrame() {
         setTitle("Relative Clique Width");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 797, 299);
+        setBounds(100, 100, 797, 336);
         
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
@@ -100,43 +106,82 @@ public class MainFrame extends JFrame {
         textReducedTerm.setColumns(10);
         
         JButton btnLoad = new JButton("Load");
+        btnLoad.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int result = fc.showOpenDialog(me);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    DataInputStream dis;
+                    try {
+                        dis = new DataInputStream(new FileInputStream(fc.getSelectedFile()));
+                    
+                    BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+
+                    textVerNumber.setText(br.readLine());
+                    textReducedTerm.setText(br.readLine());
+                    textEdges.setText(br.readLine());
+                    
+                    dis.close();
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid File", "Alert", JOptionPane.ERROR_MESSAGE); 
+                    }
+                }
+            }
+        });
         sl_contentPane.putConstraint(SpringLayout.NORTH, btnLoad, 10, SpringLayout.NORTH, contentPane);
         sl_contentPane.putConstraint(SpringLayout.WEST, btnLoad, -80, SpringLayout.EAST, contentPane);
         sl_contentPane.putConstraint(SpringLayout.EAST, btnLoad, -10, SpringLayout.EAST, contentPane);
         contentPane.add(btnLoad);
         
         JButton btnSave = new JButton("Save");
+        btnSave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
         sl_contentPane.putConstraint(SpringLayout.NORTH, btnSave, 7, SpringLayout.SOUTH, btnLoad);
         sl_contentPane.putConstraint(SpringLayout.WEST, btnSave, -80, SpringLayout.EAST, contentPane);
         sl_contentPane.putConstraint(SpringLayout.EAST, btnSave, -10, SpringLayout.EAST, contentPane);
         contentPane.add(btnSave);
         
         JButton btnProcess = new JButton("Process");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, btnProcess, 24, SpringLayout.SOUTH, textReducedTerm);
+        sl_contentPane.putConstraint(SpringLayout.WEST, btnProcess, -197, SpringLayout.EAST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.EAST, btnProcess, -108, SpringLayout.EAST, contentPane);
         btnProcess.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     Term term = Main.constructTerm(textReducedTerm.getText(), textEdges.getText());
                     textTerm.setText(term.toString());
+                    textUsedLabels.setText(Integer.toString(term.getUsedLabels().size()));
                 } catch (RcwException e1) {
                     JOptionPane.showMessageDialog(null, "Invalid Input", "Alert", JOptionPane.ERROR_MESSAGE); 
                 }
             }
         });
-        sl_contentPane.putConstraint(SpringLayout.WEST, btnProcess, -197, SpringLayout.EAST, contentPane);
-        sl_contentPane.putConstraint(SpringLayout.EAST, btnProcess, 0, SpringLayout.EAST, textEdges);
         contentPane.add(btnProcess);
         
         JLabel Term = new JLabel("Term:");
         sl_contentPane.putConstraint(SpringLayout.WEST, Term, 0, SpringLayout.WEST, lblNewLabel);
-        sl_contentPane.putConstraint(SpringLayout.SOUTH, Term, -37, SpringLayout.SOUTH, contentPane);
         contentPane.add(Term);
         
         textTerm = new JTextField();
-        sl_contentPane.putConstraint(SpringLayout.SOUTH, btnProcess, -15, SpringLayout.NORTH, textTerm);
-        sl_contentPane.putConstraint(SpringLayout.NORTH, textTerm, -3, SpringLayout.NORTH, Term);
+        sl_contentPane.putConstraint(SpringLayout.NORTH, Term, 3, SpringLayout.NORTH, textTerm);
+        sl_contentPane.putConstraint(SpringLayout.NORTH, textTerm, 17, SpringLayout.SOUTH, btnProcess);
         sl_contentPane.putConstraint(SpringLayout.WEST, textTerm, 0, SpringLayout.WEST, textVerNumber);
         sl_contentPane.putConstraint(SpringLayout.EAST, textTerm, 0, SpringLayout.EAST, textEdges);
         contentPane.add(textTerm);
         textTerm.setColumns(10);
+        
+        JLabel lblNewLabel_3 = new JLabel("Number of labels");
+        sl_contentPane.putConstraint(SpringLayout.NORTH, lblNewLabel_3, 30, SpringLayout.SOUTH, Term);
+        sl_contentPane.putConstraint(SpringLayout.WEST, lblNewLabel_3, 0, SpringLayout.WEST, lblNewLabel);
+        contentPane.add(lblNewLabel_3);
+        
+        textUsedLabels = new JTextField();
+        sl_contentPane.putConstraint(SpringLayout.NORTH, textUsedLabels, -3, SpringLayout.NORTH, lblNewLabel_3);
+        sl_contentPane.putConstraint(SpringLayout.WEST, textUsedLabels, 0, SpringLayout.WEST, textVerNumber);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, textUsedLabels, -12, SpringLayout.SOUTH, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.EAST, textUsedLabels, 0 , SpringLayout.EAST, textEdges);
+        contentPane.add(textUsedLabels);
+        textUsedLabels.setColumns(10);
     }
 }
